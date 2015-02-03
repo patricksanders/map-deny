ca666a74639145b638a88c2caa81bdf777d51b6a
patricksanders <mail@patricksanders.net>
2015-02-02 19:29:29 -0600

    building out flask functionality

---
+++ config_SAMPLE.py
@@ -0,0 +1,2 @@
+DEBUG = True
+UPLOAD_FOLDER = '/Users/patrick/dev/map-deny/upload'
--- map-deny.py
+++ map-deny.py
@@ -1,10 +1,36 @@
-from flask import Flask, render_template
+import os
+from flask import Flask, render_template, redirect, request, send_from_directory, url_for
+from werkzeug import secure_filename
+import config
+
 app = Flask(__name__)
-DEBUG = True
+app.config['UPLOAD_FOLDER'] = config.UPLOAD_FOLDER
 
 @app.route('/')
 def main_page():
 	return render_template('index.html')
 
+@app.route('/upload', methods=['GET', 'POST'])
+def upload_file():
+	if request.method == 'POST':
+		uploaded_file = request.files['file']
+		if uploaded_file:
+			filename = secure_filename(uploaded_file.filename)
+			uploaded_file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+			return redirect(url_for('serve_file', filename=filename))
+	return '''
+	<!doctype html>
+	<title>Upload new File</title>
+	<h1>Upload new File</h1>
+	<form action="" method=post enctype=multipart/form-data>
+		<p><input type=file name=file>
+			<input type=submit value=Upload>
+	</form>
+	'''
+
+@app.route('/uploads/<filename>')
+def serve_file(filename):
+	return send_from_directory(app.config['UPLOAD_FOLDER'], filename)
+
 if __name__ == '__main__':
-	app.run(debug=DEBUG)
+	app.run(debug=config.DEBUG)
