85b9fc02894ee0182d99514cde410c7afff0a484
patricksanders <mail@patricksanders.net>
2015-03-07 23:09:28 -0500

    changed to better-looking template

--- map-deny.py
+++ map-deny.py
@@ -1,14 +1,19 @@
 import os
 from flask import Flask, render_template, redirect, request, send_from_directory, url_for
+from flask_debugtoolbar import DebugToolbarExtension
 from werkzeug import secure_filename
 import config
 
 app = Flask(__name__)
+toolbar = DebugToolbarExtension(app)
+app.debug = config.DEBUG
 app.config['UPLOAD_FOLDER'] = config.UPLOAD_FOLDER
+app.config['SECRET_KEY'] = config.SECRET_KEY
+
 
 @app.route('/')
 def main_page():
-	return render_template('index.html')
+	return render_template('map.html')
 
 @app.route('/upload', methods=['GET', 'POST'])
 def upload_file():
@@ -33,4 +38,4 @@ def serve_file(filename):
 	return send_from_directory(app.config['UPLOAD_FOLDER'], filename)
 
 if __name__ == '__main__':
-	app.run(debug=config.DEBUG)
+	app.run()
